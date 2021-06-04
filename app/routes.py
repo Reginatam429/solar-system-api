37fbc2f6536c990987db0d002cc93c74f5c53f3b
Regina <reginatam429@gmail.com>
2021-06-04 15:07:56 -0700

    adds endpoint for getting all planets

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,6 @@
-from flask import Blueprint
 from app import db
 from app.models.planet import Planet
-from flask import request, Blueprint, make_response
+from flask import request, Blueprint, make_response, jsonify
 
 hello_world_bp = Blueprint("hello_world", __name__)
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
@@ -12,13 +11,24 @@ def endpoint_name():
     my_beautiful_response_body = "Hello, World!"
     return my_beautiful_response_body
 
-@planets_bp.route("", methods=["POST"])
+@planets_bp.route("", methods=["GET","POST"])
 def handle_planets():
-    request_body = request.get_json()
-    new_planet = Planet(planet_name=request_body["planet_name"],
-                    description=request_body["description"])
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append({
+                "id": planet.id,
+                "planet_name": planet.planet_name,
+                "description": planet.description
+            })
+        return jsonify(planets_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_planet = Planet(planet_name=request_body["planet_name"],
+                        description=request_body["description"])
 
-    db.session.add(new_planet)
-    db.session.commit()
+        db.session.add(new_planet)
+        db.session.commit()
 
     return make_response(f"Planet {new_planet.planet_name} successfully created", 201)
